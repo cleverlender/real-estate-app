fe350488969cdf519e46fc68d34b576fd20d3855
Bryant Mateus <bryant.mateus@gmail.com>
2023-10-17 16:08:03 -0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// initialize dotenv (aka start it up)
+dotenv.config();
+
+// connect to the MongoDB database we created
+// actual URL is saved in a process dot env file called MONGO
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
