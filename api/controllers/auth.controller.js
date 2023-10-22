54b54b00e677b82a8887c4031db5c1c8f257afc7
Bryant Mateus <bryant.mateus@gmail.com>
2023-10-22 10:34:41 -0500

    create sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   // destructure and save the username/email/passwords from the req.body (insomnia) into their respective variables
@@ -21,6 +22,30 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created successfully!");
   } catch (error) {
     // Create a status response that an error happened (500)
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if this email already exists
+    const validUser = await User.findOne({ email });
+
+    // Check credentials
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, "Wrong e-mail or password!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
