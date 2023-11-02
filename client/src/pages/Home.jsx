e8945c28bb9171d7d47faf7b1858d601bd4b58f9
Bryant Mateus <bryant.mateus@gmail.com>
2023-11-02 11:06:00 -0500

    more Home UI changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,7 +50,7 @@ export default function Home() {
   return (
     <div>
       {/* top  */}
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+      <div className="flex flex-col gap-6 p-20 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl ">
           Find your next <span className="text-slate-500">perfect</span>
           <br />
@@ -66,7 +66,7 @@ export default function Home() {
         <Link
           to={"/search"}
           className="text-xs sm:text-sm text-blue-800 font-bold hover:underline">
-          Let's get started...
+          Click here to begin your search!
         </Link>
       </div>
 
