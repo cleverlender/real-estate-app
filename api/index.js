f5094a5822ce8b51400ee0b3b3cabb507cace90b
Bryant Mateus <bryant.mateus@gmail.com>
2023-11-02 09:26:05 -0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // initialize dotenv (aka start it up)
 dotenv.config();
@@ -19,6 +20,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -38,6 +41,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // Create a middleware that will take error (error coming from input), request (data from browser/client),
 // res (response from server side) and next (to go to the next middleware)
